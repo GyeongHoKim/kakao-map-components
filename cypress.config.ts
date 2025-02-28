070a9fd431d2641f2fd46b77e28d9852a7441585
GyeongHo Kim <rlarudgh2017@gmail.com>
2025-03-01 00:11:31 +0900

    test: Kakao Map API Key Env setting & E2E test of Loader
    - Cypress 설정에 카카오 맵 API 키 환경 변수 추가
    - GitHub Actions 워크플로우 생성 (린트, 빌드, E2E 테스트)
    - 카카오 객체 로드 테스트 케이스 추가

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,9 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
     specPattern: "cypress/e2e/**/*.cy.{ts,tsx}",
-    chromeWebSecurity: false,
+    env: {
+      appkey: process.env.KAKAO_MAP_API_KEY,
+    },
   },
   includeShadowDom: true,
 });
