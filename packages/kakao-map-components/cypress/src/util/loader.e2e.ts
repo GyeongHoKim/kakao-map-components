ca17bea94d583c5539fc3b484283462e18144e73
GyeongHo Kim <rlarudgh2017@gmail.com>
2025-03-06 21:44:12 +0900

    [kakao-map-components] test: added component test and modified comments
    - Cypress E2E 테스트에 arrange, act, assert 주석 추가
    - Api loader 컴포넌트 테스트 코드 추가

--- packages/kakao-map-components/cypress/src/util/loader.e2e.ts
+++ packages/kakao-map-components/cypress/src/util/loader.e2e.ts
@@ -1,7 +1,9 @@
 describe("Loader class", () => {
   it("should return kakao object after load method", () => {
+    // arrange
     const appkey = Cypress.env("appkey");
     cy.visit(`/cypress/src/util/index.html?appkey=${appkey}`);
+    // act & assert
     cy.window().then((win) => {
       cy.wrap(win).its("kakao").should("exist");
     });
